0fa4c01faf819c25bcfa73bc27bebf5b4bebb24c
Chandika Herath <chandikasbh@gmail.com>
2024-02-28 21:59:28 +1300

    Create the page object model and updated edit and delete flows. Added the WaitUtil class for different waits

---
+++ February2024/Pages/HomePage.cs
@@ -0,0 +1,38 @@
+﻿using February2024.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace February2024.Pages
+{
+    public class HomePage
+    {
+        public void NavigateToTMPage(IWebDriver driver)
+        {
+            //Navigate to Time and Material module (Click on the Administration Dropdown Menu link)
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("//*[contains(text(),'Administration')]"));
+            administrationDropdown.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a")));
+
+            WaitUtils.WaitToBeVisible(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 3);
+
+            IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            timeAndMaterialOption.Click();
+        }
+
+        public void VerifyLoggedInUser(IWebDriver driver)
+        {
+            //Check if the user has logged in successfully
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+            if (helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("User has logged in successfully");
+            }
+            else
+            {
+                Console.WriteLine("User hasn't been logged in :( :( :(");
+            }
+        }
+    }
+}
--- February2024/Program.cs
+++ February2024/Program.cs
@@ -1,89 +1,26 @@
-﻿using OpenQA.Selenium;
+﻿using February2024.Pages;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-//Open Chrome/Firefox browser
-IWebDriver driver = new ChromeDriver();
-driver.Manage().Window.Maximize();
-
-//Launch TurnUp portal and navigate to website login page
-driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
-
-//Identify username textbox and enter valid username
-IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
-usernameTextbox.SendKeys("hari");
-
-//Identify password textbox and enter password
-IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
-passwordTextbox.SendKeys("123123");
-
-//Just to wait for 5 seconds doing nothing
-Thread.Sleep(2000);
-
-//Identify login button and click on Login Button
-IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
-loginButton.Click();
-
-
-
-//Check if the user has logged in successfully
-IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
-if (helloHari.Text == "Hello hari!")
+public class Program
 {
-    Console.WriteLine("User has logged in successfully");
-}
-else
-{ 
-    Console.WriteLine("User hasn't been logged in :( :( :(");
-}
+    private static void Main(string[] args)
+    {
+        //Open Chrome/Firefox browser
+        IWebDriver driver = new ChromeDriver();
 
-//=================================================================
+        LoginPage loginPageObj = new LoginPage();
+        loginPageObj.LoginActions(driver, "hari", "123123");
 
-//Create a new Time record
+        HomePage homePageObj = new HomePage();
+        homePageObj.VerifyLoggedInUser(driver);
+        homePageObj.NavigateToTMPage(driver);
 
-//Navigate to Time and Material module (Click on the Administration Dropdown Menu link)
-IWebElement administrationDropdown = driver.FindElement(By.XPath("//*[contains(text(),'Administration')]"));
-administrationDropdown.Click();
-IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
-timeAndMaterialOption.Click();
+        TimeMaterialPage timeMaterialPageObj = new TimeMaterialPage();
+        timeMaterialPageObj.CreateTimeMaterialRecord(driver);
 
-//Click on Create new button
-IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
-createNewButton.Click();
+        timeMaterialPageObj.EditTimeMaterialRecord(driver);
 
-//Select Time from dropdown
-IWebElement typeCodeMainDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span"));
-typeCodeMainDropdown.Click();
-IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
-typeCodeDropdown.Click();
-
-//Enter Code
-IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
-codeTextbox.SendKeys("February2024");
-
-//Enter Description
-IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
-descriptionTextbox.SendKeys("February2024Description");
-
-//Enter the price
-IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
-priceTextbox.SendKeys("25");
-
-//Click on the Save Button
-IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
-saveButton.Click();
-
-Thread.Sleep(2000);
-
-//Check if a new time record has been created successfully
-IWebElement goToLastpageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
-goToLastpageButton.Click();
-
-IWebElement newRecordCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
-if (newRecordCode.Text == "February2024")
-{
-    Console.WriteLine("New Material/Time Record has been created successfully");
-}
-else
-{
-    Console.WriteLine("New Material/Time Record has not been created :( :( :(");
+        timeMaterialPageObj.DeleteTimeMaterialRecord(driver);
+    }
 }
